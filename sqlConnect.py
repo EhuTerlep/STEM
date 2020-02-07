260b8ee516a002a83a7427e5fa268d78a4cba1c5
imasavage <42422235+EhuTerlep@users.noreply.github.com>
2020-02-07 09:18:21 -1000

    Update sqlConnect.py

--- sqlConnect.py
+++ sqlConnect.py
@@ -5,5 +5,13 @@ cur = db.cursor(MySQLdb.cursors.DictCursor)
 
 db.autocommit(True)
 
+sql = "SELECT * from students"
+cur.execute(sql)
+
+#display data in db
+rows = cur.fetchall()
+for row in rows:
+    print("Hi " + row['name'] + ", you are " + str(row['age']) + " years old and in grade " + str(row['gradeLevel']))
+
 cur.close
 db.close
