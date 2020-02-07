de59c7f37086ffa64b74a72abb27125bf619289d
imasavage <42422235+EhuTerlep@users.noreply.github.com>
2020-02-07 08:51:37 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -3,9 +3,6 @@ $servername = "localhost";
 $username = "user";
 $password = "password";
 $dbname = "school";
-$name = 'Test';
-$age = 15;
-$gradeLevel = 11;
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
