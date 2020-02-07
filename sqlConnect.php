449aa62c767eda5b189c2854e2468c68dbf0bf59
imasavage <42422235+EhuTerlep@users.noreply.github.com>
2020-02-07 09:23:10 -1000

    Update sqlConnect.php

--- sqlConnect.php
+++ sqlConnect.php
@@ -9,3 +9,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name: " . $row["name"]. " Grade: " . $row["gradeLevel"]. " age: " . $row["age"] "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
