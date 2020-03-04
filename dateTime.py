4415ec0b24e453714bc2de10dc0be91b50d6af41
imasavage <42422235+EhuTerlep@users.noreply.github.com>
2020-03-04 08:34:39 -1000

    Add files via upload

---
+++ dateTime.py
@@ -0,0 +1,29 @@
+# Use the datetime object to create and manipulate date and time
+
+#Import modules needed from datetime
+from datetime import time
+from datetime import date
+from datetime import datetime
+
+#create a main loop so this module can be imported
+def main():
+	#create a new date time object that holds the current datetime
+	currentTime = datetime.now()
+	
+	print(currentTime)
+	
+	#Print only the time from the datetime object
+	print(datetime.time(currentTime))
+	
+	#Use strftime to print only the year from the datetime object
+	print("Current Year: ",currentTime.strftime("%Y"))
+	
+	#Use strftime  to print a very human readable
+	print("Current Year: ",currentTime.strftime("%a, B, %d, %Y"))
+
+
+
+
+
+if __name__ == "__main__":
+	main()
